f7757f021c61436a6e9cf97cee166e8bcab0b728
Mark Brighton <marik.shevchenko@icloud.com>
2021-04-21 13:56:41 +0300

    commit 0.2 Add TextConnectorProcessor and TextConnector classes.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
 
         // TODO - Wire up the CreatePrizefor the text files
         public PrizeModel CreatePrize(PrizeModel model)
         {
-          // Load the text file
-          //Convert the text to Liast<PrizeModel>
-          //Find the ID
-          //Add the new record with the new ID(max + 1)
-          //Convert the prizes to list<string>
-          //Save the list<string> to the text file
+            //Load th etext file and convert to List<PrizeModels>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            //Find the max ID
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+
+            // Add the 
+            prizes.Add(model);
+
+            // Convert the prizes to list<string>
+            // Save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
+
+            return model;
         }
     }
 }
