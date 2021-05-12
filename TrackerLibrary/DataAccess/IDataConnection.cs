1c684559a3b5a59eef24763c2b48ef5ba37bb634
Mark Brighton <marik.shevchenko@icloud.com>
2021-05-12 16:16:13 +0300

    commit 7.1 (Added Id row to TournamentModel)

--- TrackerLibrary/DataAccess/IDataConnection.cs
+++ TrackerLibrary/DataAccess/IDataConnection.cs
@@ -11,6 +11,9 @@ namespace TrackerLibrary
         PrizeModel CreatePrize(PrizeModel model);
         PersonModel CreatePerson(PersonModel model);
         TeamModel CreateTeam(TeamModel model);
+        TournamentModel CreateTournament(TournamentModel model);
+
+
         List<TeamModel> GetTeam_All();
         List<PersonModel> GetPerson_All();
 
